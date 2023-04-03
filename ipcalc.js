03adff8a550cf4dd658cb0ed2f202d4ac258281a
Baptiste Dromer <baptiste.dromer@gmail.com>
2023-04-03 15:34:49 +0200

    minor refactor

--- ipcalc.js
+++ ipcalc.js
@@ -1,69 +1,53 @@
-const calcNetwork = (addr, netmask) => {
-  // return the network address
-  // & -> bitwise AND
-  return addr & netmask;
-};
+// returns the network address
+// & -> bitwise AND
+const calcNetwork = (addr, netmask) => addr & netmask;
 
-const calcBroadcast = (network, netmask) => {
-  // return the broadcast address
-  // | -> bitwise OR
-  // ~ -> bitwise complement (NOT), inverts all bits 0101 -> 1010
-  return network | ~netmask;
-};
+// returns the broadcast address
+// | -> bitwise OR
+// ~ -> bitwise complement (NOT), inverts all bits 0101 -> 1010
+const calcBroadcast = (network, netmask) => network | ~netmask;
 
-const calcWildcard = (netmask) => {
-  // return the wildcard mask (inverted netmask)
-  // 255.255.255.0 -> 255.0.0.0
-  return ~netmask;
-};
+// returns the wildcard mask (inverted netmask)
+// 255.255.255.0 -> 255.0.0.0
+const calcWildcard = (netmask) => ~netmask;
 
-const calcFirstAddr = (network) => {
-  // return first assignable address
-  return network + 1;
-};
+// returns first assignable address
+const calcFirstAddr = (network) => network + 1;
 
-const calcLastAddr = (network, netmask) => {
-  // return last assignable address
-  return calcBroadcast(network, netmask) - 1;
-};
+// returns last assignable address
+const calcLastAddr = (network, netmask) => calcBroadcast(network, netmask) - 1;
 
-const calcRangeLength = (netmask) => {
-  // return full range length
-  return ~netmask - 1;
-};
+// returns full range length
+const calcRangeLength = (netmask) => ~netmask - 1;
 
-const addrToNumber = (addr) => {
-  // convert ipv4 address from String in dot-decimal notation to Number
-  // 192.168.0.1 -> 3232235521
-  return addr
+// converts ipv4 address from String in dot-decimal notation to Number
+// 192.168.0.1 -> 3232235521
+const addrToNumber = (addr) =>
+  addr
     .split(".")
     .reverse()
     .reduce((acc, val, i) => acc + Number(val) * 256 ** i, 0);
-};
 
+// convert ipv4 address from Number to a String in dot-decimal notation.
+// 3232235521 -> "192.168.0.1"
 const numberToAddr = (addr) => {
-  // convert ipv4 address from Number to a String in dot-decimal notation.
-  // 3232235521 -> "192.168.0.1"
   const bytes = [];
+
   bytes[0] = addr & 0xff;
   bytes[1] = (addr >>> 8) & 0xff;
   bytes[2] = (addr >>> 16) & 0xff;
   bytes[3] = (addr >>> 24) & 0xff;
 
-  return bytes[3] + "." + bytes[2] + "." + bytes[1] + "." + bytes[0];
+  return bytes.reverse().join(".");
 };
 
-const cidrToNumber = (netmask) => {
-  // convert subnetnetmask from CIDR notation to Number
-  // 24 -> 4294967040
+// convert subnetnetmask from CIDR notation to Number
+// 24 -> 4294967040
+const cidrToNumber = (netmask) => 0xffffffff << (32 - netmask);
 
-  return 0xffffffff << (32 - netmask);
-};
-
+// convert mask from Number to Number CIDR notation
+// 4294967040 -> 24 (/24)
 const numberToCidr = (netmask) => {
-  // convert mask from Number to Number CIDR notation
-  // 4294967040 -> 24 (/24)
-
   let cidr = 0;
 
   // 0x80000000 == 128.0.0.0 == /1
@@ -76,16 +60,16 @@ const numberToCidr = (netmask) => {
 
 const checkInput = (input) => {
   // returns true if input is a valid IP Address CIDR notation else return false
-  const ipv4_regex =
+  const ipv4_regexp =
     /^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$/gm;
 
   const cidr = input.split("/");
 
   if (cidr.length !== 2) return false;
 
-  if (cidr[1] === "") return false;
+  if (!ipv4_regexp.test(cidr[0])) return false;
 
-  if (!ipv4_regex.test(cidr[0])) return false;
+  if (isNaN(cidr[1])) return false;
 
   if (cidr[1] <= 0 || cidr[1] >= 32) return false;
 
@@ -145,6 +129,7 @@ document.querySelector("form").addEventListener("submit", (event) => {
   if (!checkInput(input.value)) {
     feedbackElement.textContent = "Invalid CIDR";
     setTimeout(() => (feedbackElement.textContent = ""), 5000);
+    return;
   }
 
   const ipInfo = calcIpv4(input.value);
